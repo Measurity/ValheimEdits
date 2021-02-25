007a317c07e409f6f882f765455b4552ee583d70
Measurity <measuring.infinity@gmail.com>
2021-02-25 18:05:03 +0100

    Removed ZDO send rate patch temporarily

--- Mod/Patches/ZDOManPatch.cs
+++ Mod/Patches/ZDOManPatch.cs
@@ -8,32 +8,30 @@ using ValheimEdits.Utils;
 
 namespace ValheimEdits.Patches
 {
-    /// <summary>
-    ///     Changes hardcoded data rate limit to fix lag issues.
-    /// </summary>
-    public class ZDOManPatch
-    {
-        [HarmonyPatch(typeof(ZDOMan), nameof(ZDOMan.SendZDOs))]
-        public static class SendZDOs
-        {
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-            {
-                var list = instructions.ToList();
-                for (var i = 0; i < list.Count; i++)
-                {
-                    if (i + 1 < list.Count && list[i].IsGetThis())
-                    {
-                        if (list[i + 1].IsGetField(typeof(ZDOMan), nameof(ZDOMan.m_dataPerSec)))
-                        {
-                            Debug.Log("DataRateLimit check found!");
-                            list[i].opcode = OpCodes.Nop;
-                            list[i + 1] = new CodeInstruction(OpCodes.Ldc_I4, Config.Instance.DataRateLimit);
-                        }
-                    }
-
-                    yield return list[i];
-                }
-            }
-        }
-    }
+    // TODO: Change to not be applied by default (value of <= 0 is inactive)
+    // public class ZDOManPatch
+    // {
+    //     [HarmonyPatch(typeof(ZDOMan), nameof(ZDOMan.SendZDOs))]
+    //     public static class SendZDOs
+    //     {
+    //         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //         {
+    //             var list = instructions.ToList();
+    //             for (var i = 0; i < list.Count; i++)
+    //             {
+    //                 if (i + 1 < list.Count && list[i].IsGetThis())
+    //                 {
+    //                     if (list[i + 1].IsGetField(typeof(ZDOMan), nameof(ZDOMan.m_dataPerSec)))
+    //                     {
+    //                         Debug.Log("DataRateLimit check found!");
+    //                         list[i].opcode = OpCodes.Nop;
+    //                         list[i + 1] = new CodeInstruction(OpCodes.Ldc_I4, Config.Instance.DataRateLimit);
+    //                     }
+    //                 }
+    //
+    //                 yield return list[i];
+    //             }
+    //         }
+    //     }
+    // }
 }
